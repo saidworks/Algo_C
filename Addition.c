6e21b77b352fba64400a79ab942ff2ef4c7ecbf9
Said ZITOUNI <zitouni.sd@gmail.com>
2021-01-08 12:18:58 +0100

    programme C addition

---
+++ Addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+	int a,b,sum;
+	printf("enter first number");
+	scanf("%d", &a);
+	printf("enter second number");
+	scanf("%d", &b);
+	sum = a + b ;
+	printf("%d", sum);
+	
+}
