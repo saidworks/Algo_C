4f22ecf172c0bd959f5a889ea5947ebbd10f322e
Said ZITOUNI <zitouni.sd@gmail.com>
2021-01-14 11:54:42 +0100

    new branch to test git commands

--- recursion.c
+++ recursion.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
+//declaration de fonction
+int multiply(int a,int b);
+
+	
+
+int main () {
+	int a,b;
+	printf("enter first number");
+	scanf("%d",&a);
+	printf("enter second number");
+	scanf("%d",&b);
+	multiply(a,b);
+	return 0;
+}
+
+// definition de fonction
 int multiply(int a,int b){
 
 	if (b ==0){
 		return 0; 
 	}
 	else if (b % 2 != 0){ 
-		return multiply(a-1 , b) + a;
+		return printf("%d",multiply(a-1 , b) + a);
 	}
 
 		
-	else if (b % 2 == 0){
+	else if (b % 2 == 0 && b != 0){
 		return multiply(a*2 ,b/2);
 	}
 
-}
-
-	
-
-int main (int argc, char **argv) {
-	float a,b;
-	printf("enter first number");
-	scanf("%f",&a);
-	printf("enter second number");
-	scanf("%f",&b);
-	multiply(a,b);
-	return 0;
 }
 
